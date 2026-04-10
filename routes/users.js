9ae8b2bb406437ba24e5d8a62c555f1b8d6b12e7
Maxime-Maguet <maxime.maguet@gmail.com>
2026-04-10 10:32:49 +0200

    routes signin signup completed

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,11 @@ const secret_token = process.env.JWT_SECRET;
 
 router.post(
   "/signup",
-  body("email").trim().isEmail(),
-  body("password").isLength({ min: 5 }).notEmpty(),
+  body("email").trim().isEmail().normalizeEmail().withMessage("Email invalide"),
+  body("password")
+    .isLength({ min: 8 })
+    .notEmpty()
+    .withMessage("Le mot de passe doit contenir minimun 8 characteres "),
   body("username").notEmpty(),
   async (req, res) => {
     try {
@@ -39,6 +42,56 @@ router.post(
       }
     } catch (error) {
       res.status(500).json({ result: false, error: "Erreur serveur" });
+    }
+  },
+);
+
+router.post(
+  "/signin",
+  body("password").notEmpty(),
+  body("username").notEmpty(),
+  async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      let user = await User.findOne({ username });
+      if (!user) {
+        return res.status(401).json({
+          result: false,
+          erreur: "Username ou mot de passe invalide",
+        });
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({
+          result: false,
+          error: "Username ou mot de passe invalide",
+        });
+      }
+      const token = jwt.sign({ username }, secret_token, {
+        expiresIn: "1h",
+      });
+      user = await User.findOneAndUpdate(
+        { username },
+        { $set: { token } },
+        { returnDocument: "after" },
+      );
+      const { password: hashedPassword, ...userWihoutPassword } =
+        user.toObject();
+      res.status(200).json({
+        result: true,
+        data: userWihoutPassword,
+      });
+    } catch (error) {
+      res.status(500).json({
+        result: false,
+        error: "Erreur serveur",
+      });
+      console.error(error);
     }
   },
 );

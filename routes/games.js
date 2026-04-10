a80b87004e6cf856d439a68cc92a1805c755d603
Maxime-Maguet <maxime.maguet@gmail.com>
2026-04-10 15:48:09 +0200

    route games get completed

--- routes/games.js
+++ routes/games.js
@@ -2,3 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Game = require("../models/games.js");
 module.exports = router;
+const RAWG_API_KEY = process.env.RAWG_API_KEY;
+
+router.get("/search", async (req, res) => {
+  const { page_size, search } = req.query;
+
+  try {
+    const response = await fetch(
+      `https://api.rawg.io/api/games?key=${RAWG_API_KEY}&search=${search}&page_size=${page_size}`,
+    );
+    const data = await response.json();
+
+    const gameInformations = data.results.map((data) => {
+      return {
+        rawg: data.id,
+        title: data.name,
+        released: data.released,
+        backgroundImage: data.background_image,
+        genres: data.genres,
+      };
+    });
+
+    res.json(gameInformations);
+  } catch (error) {
+    res.status(500).json({ result: false, error: "Erreur serveur" });
+  }
+});
